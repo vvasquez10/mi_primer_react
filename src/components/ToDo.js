41487cfe4068913f1362af6e488128a2be351fea
Victor Vasquez <daxos10.0@gmail.com>
2022-03-22 17:36:49 -0500

    despliegue

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+function ToDo(){
+    return (            
+        <><h2>Things I need to do</h2><ul>
+            <li>Learn React</li>
+            <li>Learn Angular</li>
+            <li>Clean my house</li>
+        </ul></>
+    );        
+}
+
+export default ToDo;
